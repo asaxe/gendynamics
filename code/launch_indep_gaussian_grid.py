54c703fa10c6b8092b7148f3f6a6a69161d05516
asaxe <asaxe@fas.harvard.edu>
2017-02-08 18:53:32 -0500

    final params

--- code/launch_indep_gaussian_grid.py
+++ code/launch_indep_gaussian_grid.py
@@ -7,10 +7,10 @@ expt = 1
 
 Ni = 784
 No = 10
-snrs = np.logspace(-1,1,1)
-alphas = np.linspace(.5,1.5,1)
+snrs = np.logspace(-1,1,7)
+alphas = np.linspace(.5,1.5,7)
 depths = [0,1]
-weightscales = np.logspace(-1,1,1)
+weightscales = np.logspace(-1,1,5)
 rseeds = np.arange(5)
 
 i = 1
@@ -20,9 +20,9 @@ for snr in snrs:
         for d in depths:
             for ws in weightscales:
                 for rseed in rseeds:
-                    fo.write("-rseed %d -weightscale %g -snr %g -lr .01 -numsamples %d -numinputs %d -numoutputs %d -depth %d -epochs 5 -savefile /n/home13/asaxe/gendynamics/results/expt%d/res%d.npz\n" % (rseed, ws, snr, P, Ni, No, d, expt, i))
+                    fo.write("-rseed %d -weightscale %g -snr %g -lr .01 -numsamples %d -numinputs %d -numoutputs %d -depth %d -epochs 5000 -savefile /n/home13/asaxe/gendynamics/results/expt%d/res%d.npz\n" % (rseed, ws, snr, P, Ni, No, d, expt, i))
                     i = i+1
                     
 fo.close()
 
-call("python run_odyssey_array.py -expt %d -mem 1500 -partition serial_requeue -paramfile params.txt -mock" % expt, shell=True)
+call("python run_odyssey_array.py -expt %d -mem 2000 -partition serial_requeue -paramfile params.txt" % expt, shell=True)
